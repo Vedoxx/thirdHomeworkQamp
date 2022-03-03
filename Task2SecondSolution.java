8483c0baeecdb58d8557be2d16cc94e30c2ab665
Vedad Karalić <61968590+Vedoxx@users.noreply.github.com>
2022-03-03 11:51:56 +0100

    Update Task2SecondSolution.java

--- Task2SecondSolution.java
+++ Task2SecondSolution.java
@@ -2,57 +2,51 @@ import java.util.Scanner;
 
 public class Task2SecondSolution {
     public static void main(String[] args) {
-        numberArray();
-    }
-    public static void numberArray() {
-        Scanner object = new Scanner(System.in);
-        int[] array = new int[50];
-        int number;
-        System.out.println("Enter numbers of the array:");
-        for(int i=0;i<array.length;i++) {
+         Scanner object = new Scanner(System.in);
+         int[] array = new int[50];
+         arrayInput(array);
+         int index = linearSearch(array);
+         arrayOutput(array,index);
+         System.out.println();
+         System.out.println("Would you like to add numbers: (y/n)");
+         System.out.print("Answer:");
+         char answer=object.next().charAt(0);
+          if(answer=='y') {
+          int[] secondArray = new int[array.length];
+          arrayInput(secondArray);
+          int indexSecond = linearSearch(secondArray);
+          arrayOutput(secondArray,indexSecond);
+          System.out.println();
+          System.out.println("All numbers combined:");
+          arrayCombined(array,index);
+          arrayCombined(secondArray,indexSecond);
+         }
+          else if(answer=='n')  System.exit(0);
+          }
+    public static void arrayInput(int[] array) {
+         Scanner object = new Scanner(System.in);
+         int number;
+         System.out.println("Enter numbers of the array:");
+        for (int i = 0; i < array.length; i++) {
             array[i] = number = object.nextInt();
             if (number == -10) {
                 break;
             }
         }
-        int index = LinearSearch(array);
+    }
+    public static void arrayOutput(int[] array ,int index) {
         System.out.println("Your numbers:");
-        for(int i=0;i<index;i++){
-            System.out.print(array[i]+" ");
+        for (int i = 0; i < index; i++) {
+            System.out.print(array[i] + " ");
         }
-        System.out.println();
-        System.out.println("Would you like to add numbers: (y/n)");
-        System.out.print("Answer:");
-         char answer=object.next().charAt(0);
-        if(answer=='y') {
-            int[] secondArray = new int[array.length];
+    }
 
-            System.out.println("Enter numbers of the array:");
-            for (int i = 0; i < secondArray.length; i++) {
-                secondArray[i] = number = object.nextInt();
-                if (number == -10) {
-                    break;
-                }
-            }
-            int indexSecond = LinearSearch(secondArray);
-            System.out.println("Your numbers :");
-            for (int i = 0; i < indexSecond; i++) {
-                System.out.print(secondArray[i]+" ");
-            }
-            System.out.println();
-            System.out.println("All numbers combined:");
-            for (int i = 0; i < index; i++) {
-                System.out.print(array[i]+" ");
-            }
-                for (int j = 0; j < indexSecond; j++) {
-                    System.out.print(secondArray[j]+" ");
-                }
+    public static void arrayCombined(int[] array,int index) {
+        for (int i = 0; i < index; i++) {
+            System.out.print(array[i] + " ");
         }
-        else if(answer=='n'){
-            System.exit(0);
-        }
-        }
-    public static int LinearSearch(int[] array) {
+    }
+    public static int linearSearch(int[] array) {
         for(int i=0;i<array.length;i++) {
             if(array[i]==-10)  return i;
         }
